59364acaaf3010832df8706f39de6e0dfe7905bc
jepapt2 <jou9225@gmail.com>
2025-04-10 16:38:29 +0900

    contentlistのwidth修正

--- src/features/content/components/ContentList.tsx
+++ src/features/content/components/ContentList.tsx
@@ -5,7 +5,7 @@ async function ContentList() {
 
   return (
     <>
-      <div className="max-w-[240px]">
+      <div className="w-[240px]">
         {contents.map((content) => (
           <a href={`/content/${content.id}`} key={content.id}>
             <div className="rounded-sm p-1 text-body hover:bg-light-bg hover:text-brand hover:cursor-pointer hover:font-bold">
